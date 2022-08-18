b6e0ed56a350b428efa07d50779fd09058b52e9f
caltaylor <Caltaylor101@gmail.com>
2022-08-18 17:46:15 -0600

    4sum comments

--- ZTM-BigTech/4SumClass.cs
+++ ZTM-BigTech/4SumClass.cs
@@ -8,22 +8,30 @@ namespace ZTM_BigTech
 {
     public class _4SumClass
     {
-        //Initialize list to return
+        //Initialize list to return that multiple methods can use.
         private List<IList<int>> result = new List<IList<int>>();
         public IList<IList<int>> FourSum(int[] nums, int target)
         {
+            //Sort the array
             Array.Sort(nums);
             //Accounts for nums.length constraint
             if (nums.Length < 4) return result;
-
+            //Initialize an index.
             int index = 0;
             while (index < nums.Length) 
             {
+                //Optional variable. It could be cast directly to the method.
+                //Passing the offset value down to the ThreeSum method.
+                //This needs to be a long so that an integer sum above int.MaxValue can be calculated. 
                 long offset = nums[index];
-                int offsetIndex = index;
+                //Optional variable. 
+                //Passing the index where the ThreeSum method will run. 
+                int offsetIndex = index + 1;
 
-                ThreeSum(nums, target, offset, offsetIndex + 1);
+                //Call the ThreeSum method on every index.
+                ThreeSum(nums, target, offset, offsetIndex);
 
+                //Find the next unique value. 
                 while (index < nums.Length && offset == nums[index])
                 {
                     index++;
@@ -33,45 +41,61 @@ namespace ZTM_BigTech
             return result;
         }
 
+        //A ThreeSum method to calculate the 4sum. 
         private void ThreeSum(int[] nums, int target, long offset, int offsetIndex)
         {
+            //Optional Variable.
+            //Renamed to make it easier to read.
             int index = offsetIndex;
+
+            //Looping everything to the right of our initial index in FourSum
             while (index < nums.Length)
             {
-                int startI = nums[index];
+                //A left and right pointer to be used to find a TwoSum
                 int left = index + 1;
                 int right = nums.Length - 1;
-                //calculate 2 sum
+
+                //A Loop to calculate the 2 sums missing. 
                 while (left < right)
                 {
+                    //Calculate the sum as a long as the int.MaxValue will be hit here. (offset was made a type long for this reason)
                     long sum = offset + nums[left] + nums[right] + nums[index];
+                    //Verify which way the pointers should move. 
+                    //Move left if the sum is < target
                     if (sum < target) left++;
+                    //Move right down if it is lower than target. 
                     else if (sum > target) right--;
-                    else if (sum == target)
+                    //Otherwise, they equal, so add everything to the result list.
+                    else
                     {
+                        //Add each of our values as a list to the result list.
                         result.Add(new List<int>() { (int)offset, nums[left], nums[right], nums[index] });
+                        //Save left and right values as a temporary value. 
                         int leftVal = nums[left];
                         int rightVal = nums[right];
+                        //Find the next unique value
                         while(left < nums.Length && leftVal == nums[left])
                         {
                             left++;
                         }
-                        while(right > 0 && rightVal == nums[right])
+                        //Find the next unique value
+                        while (right > 0 && rightVal == nums[right])
                         {
                             right--;
                         }
                     }
                 }
-                while (index < nums.Length && startI == nums[index])
+
+
+                //A tmp value used as a placeholder to find a new unique value.
+                int tmpValue = nums[index];
+                //Find a new unique value to check against.
+                while (index < nums.Length && tmpValue == nums[index])
                 {
                     index++;
                 }
             }
         }
 
-        private int CalculateTotal(int left, int leftMid, int rightMid, int right)
-        {
-            return (left + leftMid + rightMid + right);
-        }
     }
 }
--- ZTM-BigTech/Program.cs
+++ ZTM-BigTech/Program.cs
@@ -6,7 +6,6 @@ using ZTM_BigTech;
 
 Console.WriteLine("HELLO");
 
-
 _4SumClass eng = new _4SumClass();
 IList<IList<int>> list = eng.FourSum(new int[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296);
 foreach (var i in list)

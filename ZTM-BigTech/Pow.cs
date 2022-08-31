3381b28cfcd455129d320787c0f22150a65f8517
caltaylor <Caltaylor101@gmail.com>
2022-08-31 16:25:17 -0600

    power

---
+++ ZTM-BigTech/Pow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTM_BigTech
+{
+    public class Pow
+    {
+        public double MyPow(double x, int n)
+        {
+            long power = n;
+            if (power < 0)
+            {
+                power = -power;
+                x = 1 / x;
+            }
+
+            double result = 1;
+            while (power > 0) // x^13 = x^8 * x^4 * x^1   13 = (1101) ipower bipowerary
+            {
+                if (power % 2 == 1) result = result * x;
+                x = x * x;
+                power = power / 2;
+            }
+
+            return result;
+        }
+    }
+}
